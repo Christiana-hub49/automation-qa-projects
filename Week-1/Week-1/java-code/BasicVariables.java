241c3e95e51b7315b5d0de9ffb3d52cb33b4f852
Christiana-hub49 <163031075+Christiana-hub49@users.noreply.github.com>
2026-04-07 16:54:03 +0100

    Create BasicVariables.java

---
+++ Week-1/Week-1/java-code/BasicVariables.java
@@ -0,0 +1,23 @@
+public class BasicVariables {
+    public static void main(String[] args) {
+
+        // Variables and Data Types
+        int age = 25;
+        double salary = 50000.50;
+        String name = "Christiana";
+        boolean isEmployed = true;
+
+        // Output
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Employed: " + isEmployed);
+
+        // Operators
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Multiplication: " + (a * b));
+    }
+}
