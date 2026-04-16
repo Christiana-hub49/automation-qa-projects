9aaa4d0c97e5ceb60103a20ce31bbda25870c2cd
Christiana-hub49 <163031075+Christiana-hub49@users.noreply.github.com>
2026-04-16 02:46:06 +0100

    Farm.java

---
+++ Week-2/oop/Farm.java
@@ -0,0 +1,15 @@
+public class Farm {
+    public static void main(String[] args) {
+
+        Animal pig = new Pig();
+        Animal duck = new Duck();
+
+        pig.makeSound();
+        pig.eat();
+
+        System.out.println();
+
+        duck.makeSound();
+        duck.eat();
+    }
+}
