759b50648621b7be5e0ad01ca5887f98ac9c7c51
Christiana-hub49 <163031075+Christiana-hub49@users.noreply.github.com>
2026-04-16 02:42:19 +0100

    Market.java

---
+++ Week-2/oop/Market.java
@@ -0,0 +1,10 @@
+public class Market {
+    public static void main(String[] args) {
+
+        Fruit f1 = new Apple();
+        Fruit f2 = new Banana();
+
+        f1.makeJuice();
+        f2.makeJuice();
+    }
+}
